c829b6edd17ae94eae6c4a33bca1d79ebebd469c
“LucasKoritar23 <lucas.gimenes23@gmail.com>
2018-05-04 20:34:18 -0300

    CriaClasse

---
+++ features/CriaClasse.feature
@@ -0,0 +1,15 @@
+#language: pt
+#utf-8
+
+@CriaClasse
+Funcionalidade: Criar Classes
+
+Eu como usuário do MVA
+Quero preencher os dados da tela
+Para criar Ciclo
+
+Cenário: Criar Classe
+
+Dado que estaja na tela de Classe 
+Quando preencher todos os campos da tela
+Então o Sistema irá cadastrar uma classe
---
+++ features/step_definitions/CriaClasse.rb
@@ -0,0 +1,11 @@
+Dado("que estaja na tela de Classe") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando("preencher todos os campos da tela") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então("o Sistema irá cadastrar uma classe") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
